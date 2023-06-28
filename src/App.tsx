0abc8a601111cb16063efae1efe77505e588afab
dobroillya <dobroillya@gmail.com>
2023-06-28 18:46:50 +0300

    project init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+      <div className='className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 mt-8'>
+        Hello
+      </div>
+
+  )
+}
+
+export default App
