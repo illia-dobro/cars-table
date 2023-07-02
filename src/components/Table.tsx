4eba3be3eace4728dec49580d89fba594df55c87
dobroillya <dobroillya@gmail.com>
2023-07-02 11:19:04 +0300

    fix pagination

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -216,7 +216,7 @@ function Table() {
           <p className="text-sm text-gray-700">
             Showing{' '}
             <span className="font-medium">{displayCarPage - PER_PAGE + 1}</span>{' '}
-            to <span className="font-medium">{displayCarPage}</span> of{' '}
+            to <span className="font-medium">{displayCarPage <= dataLength ? displayCarPage : dataLength}</span> of{' '}
             <span className="font-medium">{dataLength}</span> results
           </p>
         </div>
